c3e6fd3db66208550a51b2ec6e31164f7b201ee6
Gary Hansen <gary@garyslittlecompany.com>
2017-05-01 16:19:53 -0600

    adding logic to summon the elevator on a request

--- elevator.js
+++ elevator.js
@@ -1,9 +1,30 @@
 
 
 
-function elevator(floors) {
+function elevator(number, floors) {
+    this.number = number;
     this.floors = floors;
+    this.occupied = 0;
+    this.currentFloor = 1;
 }
+
+elevator.prototype.reportFloor = function() {
+    console.log('Elevator 1 ' + (this.occupied == 1) ? '(occupied)' : '' + 'at floor: ' + this.currentFloor);
+} 
+
+elevator.prototype.reportDoorOpen = function() {
+    console.log('Elevator 1 at opening door at floor: ' + this.currentFloor);
+} 
+
+elevator.prototype.reportDoorClosed = function() {
+    console.log('Elevator 1 at closing door at floor: ' + this.currentFloor);
+}
+
+elevator.prototype.summon = function(start) {
+    
+}
+
+ 
 
 
 module.exports = elevator;
--- elevatorController.js
+++ elevatorController.js
@@ -4,6 +4,7 @@ var request = require('./request');
 
 
 function elevatorController(count, floors) {
+    this.floors = floors;
     this.elevators = [];
     this.requests = [];
     for (var l = 0; l < count; ++l) {
@@ -12,6 +13,22 @@ function elevatorController(count, floors) {
 }
 
 elevatorController.prototype.requestElevator = function(start, end) {
+    start = (start < 1) ? 1 : start;
+    start = (start > this.floors) ? this.floors : start;
+
+    end = (end < 1) ? 1 : end;
+    end = (end > this.floors) ? this.floors : end;
+    
+    this.requests.push(new request(start, end));
+}
+
+elevatorController.prototype.requestElevator = function(start, end) {
+    start = (start < 1) ? 1 : start;
+    start = (start > this.floors) ? this.floors : start;
+
+    end = (end < 1) ? 1 : end;
+    end = (end > this.floors) ? this.floors : end;
+    
     this.requests.push(new request(start, end));
 }
 
